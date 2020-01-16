aa19cd6f6c8456e999efcd4ca2c2b31276f3fd21
Brad <bjkapteyn@gmail.com>
2020-01-16 11:20:51 -0500

    added description of observer pattern

--- ObserverPattern/Observer.cs
+++ ObserverPattern/Observer.cs
@@ -6,5 +6,8 @@ namespace ObserverPattern
 {
     class Observer
     {
+        //The Observer Pattern defines a one-to-many dependency between objects so that when one object changes state, all of its dependents are
+        //notified and updated automatically. When the state of the 'subject' object changes, any object that is observing('observer') it will be notified and can then 
+        //update, execute etc. 
     }
 }
