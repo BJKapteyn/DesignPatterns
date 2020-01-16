5d10c132be7d15a0f6d6ddb1709f607c4c734ef6
Brad <bjkapteyn@gmail.com>
2020-01-16 12:50:25 -0500

    added subject and observer interfaces

--- ObserverPattern/Observer.cs
+++ ObserverPattern/Observer.cs
@@ -4,11 +4,26 @@ using System.Text;
 
 namespace ObserverPattern
 {
-    class Observer
+    class ObserverPat
     {
         //The Observer Pattern defines a one-to-many dependency between objects so that when one object changes state, all of its dependents are
         //notified and updated automatically. When the state of the 'subject' object changes, any object that is observing('observer') it will be notified and can then 
         //update, execute etc. These relationships are loosly coupled, or operate with little dependency on each other. This helps allow the object's continued use
         //with relative independence, and increases flexability.
+
+        //I'll start by creating a subject and observer interface.
+
+        public interface Subject
+        {
+            void AddObserver();
+            void RemoveObserver();
+            void UpdateObservers();
+        }
+
+        public interface Observer
+        {
+            void Update();
+        }
+
     }
 }
