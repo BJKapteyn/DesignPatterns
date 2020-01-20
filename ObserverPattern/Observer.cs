2e97b7d648dfffb5c4eec7915232388a5c57091c
Brad <bjkapteyn@gmail.com>
2020-01-20 14:03:49 -0500

    changed floats to decimals for rounding and added cross country class

--- ObserverPattern/Observer.cs
+++ ObserverPattern/Observer.cs
@@ -25,7 +25,8 @@ namespace ObserverPattern
 
         public interface Observer
         {
-            void Update(float snowfall);
+            string ObserverName { get; set; }
+            void Update(double snowfall);
         }
 
         //for the sake of flexability, I'm going to create an abstract class that implements the Observer interface, rather than including the snowsport's methods
@@ -33,13 +34,13 @@ namespace ObserverPattern
         //just inherit from the Observer class, and therefore the subject interface.
         public abstract class Snowsport : Observer
         {
-            string SportName;
+            public string ObserverName { get; set; }
             //I'm going to make snowfall abstract because I want IsAdvisable to change depending on what the snowfall is set as. You'll see how this works in a second.
-            public abstract float Snowfall { get; set; }
+            public abstract double Snowfall { get; set; }
             //This boolean property will tell us whether or not we should do this sport based on the snowfall. I'll auto-initialize it to false, better safe than sorry
             //when it comes to snow.
             public bool IsAdvisable { get; set; } = false;
-            public void Update(float snowfall)
+            public void Update(double snowfall)
             {
                 Snowfall = snowfall;
             }
@@ -49,9 +50,11 @@ namespace ObserverPattern
         //I'm now going to build my first snowsport that will observe snowfall.
         public class Snowboard : Snowsport
         {
-            //I don't think this sport will change anytime soon.
-            const string SportName = "Snowboarding";
-            public override float Snowfall
+            public Snowboard()
+            {
+                ObserverName = "Snowboarding";
+            }
+            public override double Snowfall
             {
                 get
                 {
@@ -61,7 +64,7 @@ namespace ObserverPattern
                 {
                     Snowfall = value;
                     //Here I can set our IsAdvisable property logic. If it snows more than 2 inches, we're going snowboarding.
-                    if(Snowfall > 2.0f)
+                    if(Snowfall > 2.0)
                     {
                         this.IsAdvisable = true;
                     }
@@ -83,12 +86,93 @@ namespace ObserverPattern
                 }
             }
         }
+        //I'll do a couple more so we can have a few observers.
+        public class IceSkating : Snowsport
+        {
+            //I don't think this sport will change anytime soon.
+            public IceSkating()
+            {
+                ObserverName = "Ice skating";
+            }
+            public override double Snowfall
+            {
+                get
+                {
+                    return Snowfall;
+                }
+                set
+                {
+                    Snowfall = value;
+                    
+                    if (Snowfall > 1.0)
+                    {
+                        IsAdvisable = false;
+                    }
+                    else
+                    {
+                        IsAdvisable = true;
+                    }
+                }
+            }
+            public override void GoOrNoGo()
+            {
+                if (IsAdvisable)
+                {
+                    Console.WriteLine("It's a good day for a triple sow-cow");
+                }
+                else
+                {
+                    Console.WriteLine("It snowed too much, wait until the snow is cleared");
+                }
+            }
+        }
+
+        public class CrossCountrySkiing : Snowsport
+        {
+            //I don't think this sport will change anytime soon.
+            public CrossCountrySkiing()
+            {
+                ObserverName = "Cross Country Skiing";
+            }
+            public override double Snowfall
+            {
+                get
+                {
+                    return Snowfall;
+                }
+                set
+                {
+                    Snowfall = value;
+
+                    if (Snowfall > 8.0)
+                    {
+                        IsAdvisable = false;
+                    }
+                    else
+                    {
+                        IsAdvisable = true;
+                    }
+                }
+            }
+            public override void GoOrNoGo()
+            {
+                if (IsAdvisable)
+                {
+                    Console.WriteLine("Let's excercise!");
+                }
+                else
+                {
+                    Console.WriteLine("It snowed too much, better wait for the trail groomers");
+                }
+            }
+        }
+
         //Next I'm going to create a measurement class as our Subject
 
         public class SnowfallMeasurement : Subject
         {
             //let's set a little bit of logic for setting the snowfall.
-            public float SnowFall
+            public double SnowFall
             {
                 get
                 {
@@ -96,20 +180,23 @@ namespace ObserverPattern
                 }
                 set
                 {
-                    this.SnowFall = value;
+                    this.SnowFall = (Math.Round(value, 2));
+                    Console.WriteLine($"It has snowed {SnowFall}");
                     //Here we go, every time we set a value for snowfall, all of the ovservers will be updated automatically. Noice.
                     UpdateObservers();
                 }
-            }
+            } 
             //our list of observers to be interated through to update.
             public List<Observer> ObserverList;
             public void AddObserver(Observer o)
             {
                 ObserverList.Add(o);
+                Console.WriteLine($"{o.ObserverName} has started observing snow-fall");
             }
             public void RemoveObserver(Observer o)
             {
                 ObserverList.Remove(o);
+                Console.WriteLine($"{o.ObserverName} has stopped observing snow-fall");
             }
             //run through all of our observers and update them.
             public void UpdateObservers()
